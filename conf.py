b0d960c8b2d25a574d139bc03dcd62cefe9d8410
Abelardo Pardo <abelardo.pardo@sydney.edu.au>
2015-01-29 17:05:53 +1100

    Exclude the Sequences/Sample folder as it has its own makefile.

--- conf.py
+++ conf.py
@@ -100,7 +100,8 @@ release = '1.0'
 # directories to ignore when looking for source files.
 exclude_patterns = [
     '_build',
-    '**/.#*.rst'
+    '**/.#*.rst',
+    'Sequences/Sample'
 ]
 
 # The reST default role (used for this markup: `text`) to use for all
