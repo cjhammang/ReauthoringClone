83744cdb85fbb692d8a273366e75aa7c8ffd98f4
Abelardo Pardo <abelardo.pardo@sydney.edu.au>
2015-03-05 10:51:21 +1100

    Separated include for Makefile with scripts

---
+++ Scripts/makefile_include.mk
@@ -0,0 +1,12 @@
+version_tag:
+	python $(REA_SCRIPTS)/version_tag.py
+
+rsync:
+	python $(REA_SCRIPTS)/rsync.py
+
+sanity_check:
+	python $(REA_SCRIPTS)/sanity_check.py
+
+link_checkedr:
+	python $(REA_SCRIPTS)/link_checker.py
+
