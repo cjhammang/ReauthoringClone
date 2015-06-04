43472be0193ba5ca3624d7a25ad515d170b8a31a
Abelardo Pardo <abelardo.pardo@sydney.edu.au>
2015-06-05 08:30:07 +1000

    Changed default paper size for latex to A4

--- conf.py
+++ conf.py
@@ -258,7 +258,7 @@ if tags.has('iguide'):
 
 latex_elements = {
 # The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+'papersize': 'a4paper',
 
 # The font size ('10pt', '11pt' or '12pt').
 #'pointsize': '10pt',
